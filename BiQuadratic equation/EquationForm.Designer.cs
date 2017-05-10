5dfe01e733566031513a0aeb7e3afb47926b31dd
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-05-10 22:04:58 +0300

    BiQuadratic equation

---
+++ BiQuadratic equation/EquationForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BiQuadratic_equation
+{
+    partial class EquationForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(127, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Біквадратне рівняння";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxC.Location = new System.Drawing.Point(296, 84);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(63, 23);
+            this.textBoxC.TabIndex = 1;
+            this.textBoxC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OnlyNumber);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxB.Location = new System.Drawing.Point(188, 84);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(63, 23);
+            this.textBoxB.TabIndex = 1;
+            this.textBoxB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OnlyNumber);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxA.Location = new System.Drawing.Point(78, 84);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(63, 23);
+            this.textBoxA.TabIndex = 1;
+            this.textBoxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OnlyNumber);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClose.Location = new System.Drawing.Point(339, 152);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(109, 35);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Вихід";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(217, 152);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(109, 35);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Порахувати";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(146, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "x^4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(257, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "x^2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(363, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "= 0";
+            // 
+            // EquationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 202);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.label1);
+            this.Name = "EquationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Біквадратне рівняння";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BiQuadratic equation/EquationForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BiQuadratic_equation
+{
+    public partial class EquationForm : Form
+    {
+        delegate bool IsEmpty(string str);
+        IsEmpty isEmpty = String.IsNullOrEmpty;
+        public EquationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OnlyNumber(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != ',' && e.KeyChar != '.' && 
+                e.KeyChar != (char)8 && e.KeyChar != '-')
+                e.KeyChar = (char)0;
+            else if (e.KeyChar == '.')
+                e.KeyChar = ',';
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if(isEmpty(textBoxA.Text) && isEmpty(textBoxB.Text) && isEmpty(textBoxC.Text))
+            {
+                MessageBox.Show("Заповніть всі поля",
+                    "Попередження",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+            double a = Double.Parse(textBoxA.Text);
+            double b = Double.Parse(textBoxB.Text);
+            double c = Double.Parse(textBoxC.Text);
+
+            double D = b * b - 4 * a * c;
+            if(D == 0)
+            {
+                MessageBox.Show("Рівняння не має розв\'язку",
+                    "Відповідь",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                return;
+            }
+
+            double x1 = (-b - Math.Sqrt(D)) / (2 * a);
+            double x2 = (-b + Math.Sqrt(D)) / (2 * a);
+            string message = "Корені рівняння: ";
+            bool sw = false;
+
+            if(x1 > 0)
+            {
+                sw = true;
+                x1 = Math.Sqrt(x1);
+                message += String.Format("{0}, {1},", x1, -x1);
+            }
+            if (x2 > 0)
+            {
+                sw = true;
+                x2 = Math.Sqrt(x2);
+                message += String.Format("{0}, {1}", x2, -x2);
+            }
+            if(x1 == 0 || x2 == 0)
+            {
+                sw = true;
+                message += ", 0";
+            }
+
+            if (!sw)
+            {
+                MessageBox.Show(message + "коренів не має",
+                "Відповідь",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            }
+            else MessageBox.Show(message,
+                "Відповідь",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
