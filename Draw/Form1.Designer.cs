045d1667559a45b7c6af95495b8e76a6a76902a0
Grimm <andrey.grimm.1998@gmail.com>
2017-12-25 16:21:34 +0200

    add draw

---
+++ Draw/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.Color.White;
+            this.picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picture.Location = new System.Drawing.Point(0, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(584, 361);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.picture);
+            this.Name = "Form1";
+            this.Text = "Draw";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
+
---
+++ Draw/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            Pen pen = new Pen(Color.Black, 3);
+            g.DrawRectangle(pen, new Rectangle(new Point(150, 100), new Size(200, 150)));
+            g.DrawLine(pen, new Point(150, 100), new Point(200, 50));
+            g.DrawLine(pen, new Point(350, 100), new Point(300, 50));
+            g.DrawLine(pen, new Point(200, 50), new Point(300, 50));
+            g.DrawRectangle(pen, new Rectangle(new Point(225, 150), new Size(50, 50)));
+
+            picture.Image = bmp;
+        }
+    }
+}
